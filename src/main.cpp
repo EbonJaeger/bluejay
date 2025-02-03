74011e8b34c59070194ec5fde0d771cd64a5d731
Evan Maddock <maddock.evan@vivaldi.net>
2025-02-03 13:41:47 -0500

    Make project REUSE compliant
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,10 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <https://mozilla.org/MPL/2.0/>.
+ *
+ * SPDX-FileCopyrightText: Evan Maddock <maddock.evan@vivaldi.net>
+ *
+ * SPDX-License-Identifier: MPL-2.0
  */
 
 #include <BluezQt/InitManagerJob>
